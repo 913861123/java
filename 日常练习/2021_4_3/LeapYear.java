cb6d4ccb5a32246123fcd74cf399d38452de1bbc
何翰宇 <913861123@qq.com>
2021-04-03 11:34:11 +0800

    输出1000到2000之间的所有闰年

---
+++ 日常练习/2021_4_3/LeapYear.java
@@ -0,0 +1,11 @@
+public class LeapYear {
+
+    public static void main(String[] args) {
+        int i = 0;
+        for(i=1000;i<=2000;i++){
+            if((i%4==0&&i%100!=0)||(i%400==0)){
+                System.out.println(i);
+            }
+        }
+    }
+}
