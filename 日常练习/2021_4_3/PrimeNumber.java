b3ecca6c5271a5f11dd18dee5842a255e18378b2
何翰宇 <913861123@qq.com>
2021-04-14 13:33:34 +0800

    输出100到200之间的所有素数

--- 日常练习/2021_4_3/PrimeNumber.java
+++ 日常练习/2021_4_3/PrimeNumber.java
@@ -5,7 +5,7 @@ public class PrimeNumber {
         int j = 0;
         int flag = 0;
         for(i=100;i<=200;i++){
-            for(j=2;j<Math.sqrt(i);j++){//如果i是素数那么2到根号i中一定有个数字能整除i
+            for(j=2;j<=Math.sqrt(i);j++){//如果i是素数那么2到根号i中一定有个数字能整除i
                 if(i%j==0){
                     flag = 1;
                     break;
