f21fdf149dcd8c181dbc37f0532d4e3a45b147ef
何翰宇 <913861123@qq.com>
2021-04-19 20:25:33 +0800

    实现一个方法 printArray, 以数组为参数, 循环访问数组中的每个元素, 打印每个元素的值

---
+++ 日常练习/2021_4_19/TestArray.java
@@ -0,0 +1,13 @@
+public class TestArray{
+    public static void printArray(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,3,5,7,9};
+        printArray(arr);
+    }
+}
+
