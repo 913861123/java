875c678c6f8b9468f4330b1de84937ac9d241303
何翰宇 <913861123@qq.com>
2021-04-19 20:19:50 +0800

    有一组数据，只有一个数字是出现一次，其他是两次。找出出现一次的数字

--- 日常练习/2021_4_16/SingleAf.java
+++ 日常练习/2021_4_16/SingleAf.java
@@ -4,21 +4,17 @@ public class SingleAf {
      * 找出单身狗
      * @param arr
      */
-    public static void singleaf(int[] arr){
-        for (int i = 0; i < arr.length-1; i++) {
-            for (int j = i+1; j < arr.length; j++) {
-                arr[i] = arr[i]^arr[j];
+    public static int singleaf(int[] arr){
+            int tmp = 0;
+            for (int i = 0; i < arr.length; i++) {
+                tmp^=arr[i];
             }
-        }
-        for (int i = 0; i < arr.length; i++) {
-            if(arr[i]!=0){
-                System.out.println("单身狗为 "+arr[i]);
-                break;
-            }
-        }
+
+            return tmp;
     }
-    public static void main(String[] args) {
-        int[] arr = {1,8,1,2,2,3,3,4,4,5,5,6,6,};
-        singleaf(arr);
-    }
+
+        public static void main(String[] args) {
+            int[] arr = {1,1,2,2,5,6,6};
+            System.out.println(singleaf(arr));
+        }
 }
